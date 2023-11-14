661a75c313694419b70cd108018145fb0cc01306
Grant <gkinghorn3@icloud.com>
2023-11-14 16:59:59 +0000

    logic for showing questions started & displaying questions in random order

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 
+import QUESTIONS from "../../questions";
 
 export default function Quiz() {
-    return (
+  const [userAnswers, setUserAnswers] = useState([]);
 
-    )
+  const ActiveQuestionIndex = userAnswers.length;
+
+  return (
+    <div id="question">
+      <h2>{QUESTION[ActiveQuestionIndex].text}</h2>
+
+      <ul>
+        {QUESTIONS[ActiveQuestionIndex].answers.map((answer) => (
+          <li>{answer}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
