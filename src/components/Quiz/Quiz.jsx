521a449fb1c51adf6c2c2bfa788a24a48a8f7520
Grant <gkinghorn3@icloud.com>
2023-11-15 11:54:41 +0000

    Fisher-Yates shuffle algo used to replace sort for randomising answers

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -3,9 +3,29 @@ import { useState } from "react";
 import QUESTIONS from "../../questions";
 
 export default function Quiz() {
+
+  
+
   const [userAnswers, setUserAnswers] = useState([]);
 
   const ActiveQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...QUESTIONS[ActiveQuestionIndex].answers]; 
+  // shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  // using Fisher-Yates shuffle algorithm
+  const shuffleQuestions = (array) => {
+    // Loop over the array from the last element to the first
+    for (let i = array.length - 1; i > 0; i--) {
+      // Pick a random index from 0 to i
+      let j = Math.floor (Math.random () * (i + 1));
+      // Swap the elements at i and j
+      [array [i], array [j]] = [array [j], array [i]];
+    }
+    // Return the shuffled array
+    return array;
+  }
+
+  
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prev) => [...prev, selectedAnswer]);
@@ -17,7 +37,7 @@ export default function Quiz() {
       <h2>{QUESTIONS[ActiveQuestionIndex].text}</h2>
 
       <ul id="answers">
-        {QUESTIONS[ActiveQuestionIndex].answers.map((answer) => (
+        {shuffleQuestions(shuffledAnswers).map((answer) => (
           <li key={answer} className="answer">
             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
