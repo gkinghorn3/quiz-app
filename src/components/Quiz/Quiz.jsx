ee2cd2a45751a3e8038b648a4faa7baa37728c49
Grant <gkinghorn3@icloud.com>
2023-11-17 11:35:34 +0000

    state and logic added to check if user answer is correct or not and apply styling to buttons

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,51 +1,100 @@
 import { useState, useCallback } from "react";
 
-import { shuffleAnswers } from '../../utils';
+import { shuffleAnswers } from "../../utils";
 import QUESTIONS from "../../questions";
 import quizCompleteImg from "../../assets/quiz-complete.png";
 
-import { Timer } from '../';
+import { Timer } from "../";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const ActiveQuestionIndex = userAnswers.length;
+  // state used to monitor if user answered correctly or not 
+  const [answerState, setAnswerState] = useState("");
+
+  const ActiveQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
   const quizIsComplete = ActiveQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback((selectedAnswer) => {
-    setUserAnswers((prev) => [...prev, selectedAnswer]);
-  }, []);
+  // handle users answer
+  const handleSelectAnswer = useCallback(
+    (selectedAnswer) => {
+      setAnswerState("answered");
 
+      setUserAnswers((prev) => [...prev, selectedAnswer]);
 
-  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[ActiveQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
 
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [ActiveQuestionIndex]
+  );
+
+  // set answer to null if time runs out with no answer selected
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  // if quiz is complete show summary
   if (quizIsComplete) {
     return (
       <div id="summary">
         <img src={quizCompleteImg} alt="trophy icon" />
         <h2>Quiz Complete</h2>
-        
       </div>
     );
   }
 
+  //create new array of answers to be shuffed (leaving the intial one untouched to indentify the correct answer)
   const shuffledAnswers = [...QUESTIONS[ActiveQuestionIndex].answers];
 
   return (
     <div id="quiz">
       <div id="question">
-      <Timer key={ActiveQuestionIndex} timeOut={10000} onTimeOut={handleSkipAnswer}  />
+        <Timer
+          key={ActiveQuestionIndex}
+          timeOut={10000}
+          onTimeOut={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[ActiveQuestionIndex].text}</h2>
-        
+
         <ul id="answers">
-          {shuffleAnswers(shuffledAnswers).map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
+
+          // take shuffled answer and map them to buttons with styling dependant on answerState 
+          {shuffleAnswers(shuffledAnswers).map((answer) => {
+            let cssClasses = "";  
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+
+            // if user has answered, and isSelected is true set className to selected
+            if (answerState === 'answered' && isSelected) {
+              cssClasses = "selected";
+            }
+
+            // if user has answered, answer state is correct or wrong and is selectet, set className to correct or wrong
+            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+              cssClasses = answerState;
+            }
+
+
+            return (
+              <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)} className={cssClasses}>
                 {answer}
               </button>
-            </li>
-          ))}
+            </li>)
+
+            
+            })}
         </ul>
       </div>
     </div>
